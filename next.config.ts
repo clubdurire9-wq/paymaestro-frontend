39c494b19a4aaff782b5e60f05e2ab8d1e80d9d4
clubdurire9-wq <clubdurire9@gmail.com>
2026-07-17 13:54:11 +0200

    fix(csp): autoriser challenges.cloudflare.com dans script-src et frame-src pour le widget Turnstile

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,11 @@ const apiOrigin = new URL(apiUrl).origin;
 
 const csp = [
   "default-src 'self'",
-  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.paypal.com https://*.paypal.com https://www.google.com https://*.google.com https://accounts.google.com https://*.accounts.google.com https://www.googletagmanager.com https://*.googletagmanager.com",
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com https://www.paypal.com https://*.paypal.com https://www.google.com https://*.google.com https://accounts.google.com https://*.accounts.google.com https://www.googletagmanager.com https://*.googletagmanager.com",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "img-src 'self' data: blob: https://flagcdn.com https://*.flagcdn.com https://www.google.com https://*.google.com https://maps.google.com",
   "font-src 'self' https://fonts.gstatic.com",
-  "frame-src 'self' https://www.paypal.com https://*.paypal.com https://www.google.com https://*.google.com",
+  "frame-src 'self' https://challenges.cloudflare.com https://www.paypal.com https://*.paypal.com https://www.google.com https://*.google.com",
   `connect-src 'self' ${apiOrigin} https://*.paypal.com https://accounts.google.com https://*.googleapis.com https://flagcdn.com`,
   "form-action 'self'",
   "base-uri 'self'",
